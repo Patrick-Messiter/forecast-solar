6d09cd9a5e09cd3f581a8d8be1ae39e8dd5dde6e
Patrick Messiter <Patrick.Messiter@gmail.com>
2024-10-13 19:57:05 +1100

    Updated form widget for each field with styling and steps

--- djangoapp/estimates/forms.py
+++ djangoapp/estimates/forms.py
@@ -4,11 +4,16 @@ from djangoapp.estimates.utils import convert_floats_to_ints_if_whole
 
 
 class EstimateForm(forms.Form):
-    latitude = forms.FloatField(min_value=-90, max_value=90)
-    longitude = forms.FloatField(min_value=-180, max_value=180)
-    declination = forms.IntegerField(min_value=0, max_value=90)
-    azimuth = forms.IntegerField(min_value=-180, max_value=180)
-    kwp = forms.FloatField()
+    base_attrs = {'class': 'form-control', 'step': '1'}
+    lat_and_lon_attrs = base_attrs.copy()
+    lat_and_lon_attrs['step'] = '0.0001'
+    latitude = forms.FloatField(min_value=-90, max_value=90, widget=forms.NumberInput(attrs=lat_and_lon_attrs))
+    longitude = forms.FloatField(min_value=-180, max_value=180, widget=forms.NumberInput(attrs=lat_and_lon_attrs))
+    declination = forms.IntegerField(min_value=0, max_value=90, widget=forms.NumberInput(attrs=base_attrs))
+    azimuth = forms.IntegerField(min_value=-180, max_value=180, widget=forms.NumberInput(attrs=base_attrs))
+    kwp_attrs = base_attrs.copy()
+    kwp_attrs['step'] = '0.01'
+    kwp = forms.FloatField(widget=forms.NumberInput(attrs=kwp_attrs))
 
     def clean(self):
         cleaned_data = super().clean()
